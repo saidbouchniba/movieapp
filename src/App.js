cc051b0f2c8fcb9a31af98dfd301a87a00157bd5
saidbouchniba <saidbouchniba8@gmail.com>
2024-10-16 15:59:27 +0100

    save

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import Header from './components/Header';
 import Movielist from './components/Movielist';
 import Movies from './components/Data';
 import Addmovie from './components/Addmovie';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Moviedetails from './components/Moviedetails';
 function App() {
-  const [movies,setmovies]=useState(Movies)
-
+  const [movies, setmovies] = useState ([])
+  useEffect(()=>setmovies(Movies),[movies])
   return (
     <div className="App">
-<Header/>
-<Addmovie setmovies={setmovies} movies={movies}/>
-<Routes>
-  <Route path='/' element={<Movielist movies={movies} setmovies={setmovies}/>}/>
-  <Route path='/details/:id' element={<Moviedetails/>} />
-  
-</Routes>
+      <Header />
+      <Addmovie setmovies={setmovies} movies={movies} />
+      <Routes>
+        <Route path='/' element={<Movielist movies={movies} setmovies={setmovies} />} />
+        <Route path='/details/:id' element={<Moviedetails />} />
+
+      </Routes>
 
 
     </div>
